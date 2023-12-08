529a03432dbbeb18e59235c17a455b0ab1252a73
Brandon Townley <btownley@ctic.us>
2023-12-08 11:46:31 -0800

    Starter

---
+++ src/main/java/cti/BrandonTownley/AdventOfCode/Main.java
@@ -0,0 +1,10 @@
+package cti.BrandonTownley.AdventOfCode;
+
+// then press Enter. You can now see whitespace characters in your code.
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
